5c0d16df3b886cf7850b60f07b1cc84b0cd4ac22
ermak-work <yermalovich.m@gmail.com>
2013-04-24 19:48:46 +0300

    bash: fix namespace for commands

--- bash/_pip.sh
+++ bash/_pip.sh
@@ -1,11 +1,11 @@
-install() {
+_pip_install() {
     pip install -U -r .meta/packages --find-links=http://pypi.worldoftanks.com --index-url=http://pypi.worldoftanks.com
 }
 
 _pip() {
     case $1 in
         install)
-            install
+            _pip_install
         ;;
     esac
 }
--- bash/_python.sh
+++ bash/_python.sh
@@ -1,11 +1,11 @@
-clean() {
+_python_clean() {
     find . -type f -name "*.py[co]" -delete
 }
 
 _python() {
     case $1 in
         clean)
-            clean
+            _python_clean
         ;;
     esac
 }
