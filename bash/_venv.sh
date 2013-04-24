66fbea6a9afe575b71b61d7ed39a1365adac607d
ermak-work <yermalovich.m@gmail.com>
2013-04-24 21:54:04 +0300

    bash: add virtualenv detecting

--- bash/_venv.sh
+++ bash/_venv.sh
@@ -1,3 +1,20 @@
 _venv() {
     source $_VENV_PATH/$1/bin/activate
 }
+
+function _venv_detect {
+    current_dir=$(pwd)
+    while [ $current_dir != '/' ]; do
+        result=($(find $current_dir -maxdepth 1 -type f -name '.venvrc'))
+        if [[ ${#result[@]} > 0 ]]; then
+            result=${result[0]}
+            source $result
+            break
+        fi
+        current_dir=$(dirname $current_dir)
+    done
+}
+
+function cd {
+    builtin cd "$@" && _venv_detect
+}
