6fc442f50a1eb0a2a452f429a94a87a9d95c8c73
ermak <yermalovich.m@gmail.com>
2013-04-26 01:15:21 +0300

    refactoring bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,27 @@
 DOTFILES_PATH=$HOME/dotfiles
+BOOTSTRAP_LOCK=$DOTFILES_PATH/bootstrap.lock
 
-ln -s $DOTFILES_PATH/.tmux.conf $HOME/.tmux.conf
+require() {
+	for i in $@; do
+		if [[ -z `which $i` ]] ;then
+			echo "You should install $i!"
+			exit
+		fi
+	done
+}
+
+if [ ! -e BOOTSTRAP_LOCK ] ; then
+    touch BOOTSTRAP_LOCK
+fi
+source $BOOTSTRAP_LOCK
+
+if ! ${DOTFILES_BASHRC_INSTALLED:=false} ;then
+	echo "source $DOTFILES_PATH/.bashrc" >> $HOME/.bashrc
+	echo "DOTFILES_BASHRC_INSTALLED=true" >> $BOOTSTRAP_LOCK
+fi
+
+if ! ${DOTFILES_TMUX_CONF:=false} ;then
+	require tmux
+	ln -s $DOTFILES_PATH/.tmux.conf $HOME/.tmux.conf
+	echo "DOTFILES_TMUX_CONF=true" >> $BOOTSTRAP_LOCK
+fi
