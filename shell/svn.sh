b27e199ae3229002fc7b930d497168efe92d1e10
ermak-work <yermalovich.m@gmail.com>
2013-09-14 17:08:30 +0300

    shell: _svn diff: set default options --open

--- shell/svn.sh
+++ shell/svn.sh
@@ -6,16 +6,19 @@ function _svn() {
 }
 
 function _svn_diff() {
-    diff=$_SVN_DIFF_PATH/$_SVN_DIFF_FILE_NAME
+    local diff=$_SVN_DIFF_PATH/$_SVN_DIFF_FILE_NAME
     svn diff --git --no-diff-deleted -x -p | pygmentize -l diff -f html -O full,style=emacs > $diff
     echo "_svn diff: $diff is created."
 
     case $1 in
-        '--open') 
-chromium-browser $diff
-echo "_svn diff: $diff is opened."
-;;
+        '--open') _svn_diff_opened $diff;;
+        *) _svn_diff_opened $diff;;
     esac
+}
+
+function _svn_diff_opened() {
+    chromium-browser $1
+    echo "_svn diff: $1 is opened."
 }
 
 function _svn_remove_garbage() {
